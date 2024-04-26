4eff117a9fb4c8315a2c218b82dc1db2fbf27088
samuelmwangi729 <samuelmwangi729@gmail.com>
2024-04-26 12:16:05 +0300

    File Integrity checker completed

--- FileIntegrityChecker/fileChecker.py
+++ FileIntegrityChecker/fileChecker.py
@@ -9,10 +9,12 @@ This is done by checking their hashes after some time from a list of stored hash
 '''
 import os
 import hashlib
-import watchdog
 import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 file_hashes={}
-class FileIntegrityChecker():
+new_hashes={}
+class FileIntegrityChecker(FileSystemEventHandler):
     def __init__(self):
         self.Banner()
         print("\nLaunching the File Integrity Checker...")
@@ -48,12 +50,47 @@ class FileIntegrityChecker():
                     break
                 hasher.update(data_chunk)
         file_hashes[filename] = hasher.hexdigest()
+class RealTimeScanner(FileSystemEventHandler):
+    def on_any_event(self, event):
+        if event.is_directory:
+            return 
+        #Alert the user that the files have been modified
+        if event.event_type == 'created':
+            print(f"New file created {event.src_path}")
+        elif event.event_type == 'modified':
+            print(f"File modified: {event.src_path}")
+        elif event.event_type == 'deleted':
+            print(f"File deleted: {event.src_path}")
+    def scanner(self,folder_to_scan):
+        watch_init = RealTimeScanner()
+        watcher = Observer()
+        watcher.schedule(watch_init, folder_to_scan, recursive=True)
+        watcher.start()
 
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            watcher.stop()
+        watcher.join()
+        
 if __name__ == '__main__':
     integrityChecker = FileIntegrityChecker()
     path_to_scan = input("\nEnter the directory path\n")
+    time.sleep(1)
+    print("Please wait ...\n")
     if not path_to_scan:
-        path_to_scan="/"
+        path_to_scan="/home/p1rate/Desktop"
     integrityChecker.listContents(path_to_scan)
-    for item,value in file_hashes.items():
-        print(f"The hash for the file {item} is {value}")
+    print(f"\n Choose an Options \n =============================================\n 1. Display Hashes for files in {path_to_scan}\n 2. Monitor Directory {path_to_scan} \n 3. Exit\n")
+    ch = int(input())
+    if ch==1:
+        #show hashes here 
+        for item,value in file_hashes.items():
+            print(f"The hash for the file {item} was {value}")
+    elif ch==2:
+        rts = RealTimeScanner()
+        rts.scanner(path_to_scan) 
+    else:
+        print("[+] Exiting ......")
+        exit()
